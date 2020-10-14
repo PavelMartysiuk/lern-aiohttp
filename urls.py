09647d009516548ef2e629599d660ed2fa46b531
Pavel Martysiuk <ivan.shatuho@cogniteq.com>
2020-10-14 18:53:32 +0300

    Add User CRUD

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
-from views import create_user
+from views import create_user, update_user, delete_user, retrieve_users
 
 
 def add_urls(app):
     app.router.add_post('/user', create_user)
+    app.router.add_put('/user', update_user)
+    app.router.add_delete('/user', delete_user)
+    app.router.add_get('/user', retrieve_users)
